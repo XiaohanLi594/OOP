db4df636ce7e048c71ccabfe3b257eb7771ef431
XiaohanLi594 <2601991635@qq.com>
2024-12-12 19:05:25 +0800

    Add files via upload

---
+++ Employee.java
@@ -0,0 +1,51 @@
+package PRVMS;
+public class Employee extends Person
+{
+    private String employeeId;
+    private String position;
+    private double salary;
+
+    public Employee()
+    {
+
+    }
+
+    public Employee(String firstName, String lastName, int age, String employeeId, String position, double salary)
+    {
+        super(firstName, lastName, age);
+        this.employeeId = employeeId;
+        this.position = position;
+        this.salary = salary;
+    }
+
+    public String getEmployeeId()
+    {
+        return employeeId;
+    }
+
+    public void setEmployeeId(String employeeId)
+    {
+        this.employeeId = employeeId;
+    }
+
+    public String getPosition()
+    {
+        return position;
+    }
+
+    public void setPosition(String position)
+    {
+        this.position = position;
+    }
+
+    public double getSalary()
+    {
+        return salary;
+    }
+
+    public void setSalary(double salary)
+    {
+        this.salary = salary;
+    }
+}
+
