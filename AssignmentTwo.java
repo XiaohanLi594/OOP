0cd50c40ae93ccc186ea2aa12260f64675961e2c
XiaohanLi594 <2601991635@qq.com>
2024-12-12 19:02:44 +0800

    Add files via upload

---
+++ AssignmentTwo.java
@@ -0,0 +1,49 @@
+package PRVMS;
+public class AssignmentTwo
+{
+    public static void main(String[] args)
+    {
+        AssignmentTwo assignment = new AssignmentTwo();
+        assignment.partThree();
+        assignment.partFourA();
+        assignment.partFourB();
+        assignment.partFive();
+        assignment.partSix();
+        assignment.partSeven();
+    }
+
+    public void partThree()
+    {
+        System.out.println("Executing Part Three");
+    }
+
+    public void partFourA()
+    {
+        System.out.println("Executing Part Four A");
+    }
+
+    public void partFourB()
+    {
+        System.out.println("Executing Part Four B");
+    }
+
+    public void partFive()
+    {
+        System.out.println("Executing Part Five");
+    }
+
+    public void partSix()
+    {
+        System.out.println("Executing Part Six");
+    }
+
+    public void partSeven()
+    {
+        System.out.println("Executing Part Seven");
+    }
+}
+
+
+
+
+
